9f83668bf44df2deba8cc1cee577dbda5d74ca62
AlexRoman <mirmaker181@gmail.com>
2021-12-05 18:50:27 +0200

    init: start migration-newip-to-ts-task

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'e96535dbf28c4d93b4864e106f00de18', // получите свой ключ https://newsapi.org/
         });
     }
 }
