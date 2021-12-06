21d2e8bf6a5dc83db7227bd87da1d69f8a35fb08
AlexRoman <mirmaker181@gmail.com>
2021-12-06 23:26:23 +0200

    feat: style scrollbar, add media to sources

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
   constructor() {
     super('https://nodenews.herokuapp.com/', {
-      apiKey: 'e96535dbf28c4d93b4864e106f00de18&', // получите свой ключ https://newsapi.org/
+      apiKey: '2a478aabc12a43f588edf20489784bc4&', // получите свой ключ https://newsapi.org/
     });
   }
 }
